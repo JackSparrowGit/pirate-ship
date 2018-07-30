5eeb20fccf242ffa10b1fd4728163c37f72bd8f3
wupan <9874118@qq.com>
2018-07-30 22:44:36 +0800

    创建Gateway,引入docker

---
+++ pirate-gateway/src/main/java/com/sparrow/gateway/PirateGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.sparrow.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirateGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PirateGatewayApplication.class, args);
+	}
+}
